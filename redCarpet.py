2d2446278d7ccc51148e5325e12f729138a7777a
Praveen Pandey <37077527+Stenzil@users.noreply.github.com>
2019-12-22 17:06:50 +0530

    Add files via upload

---
+++ redCarpet.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 21 19:22:16 2019
+
+for doc in records.aggregate([
+    { "$match": {
+        "latitude": { "$gte": 20, "$lte": 20.15 }
+    }}
+]):
+    print(doc)
+
+db.so.create_index([("geo","2dsphere")])
+
+ db.so.find({"geo": {
+    "$near": {
+        "$geometry": {
+            "type": "Point" ,
+            "coordinates": [ 60, 22 ]
+        },
+        "$maxDistance": 80000
+    }
+}}).count()
+    
+    db.so.insert_one({
+    "geo" : {
+        "type" : "Point",
+        "coordinates" : [ 
+            61.1111, 
+            23.111123
+        ]
+    }
+})
+
+@author: xor
+"""
+
+from fastapi import FastAPI
+import pandas as pd
+from pydantic import BaseModel
+import json
+from starlette.requests import Request
+from pymongo import MongoClient
+
+
+request= Request
+app = FastAPI()
+file=pd.read_csv('IN.csv')
+k=file.values.tolist()
+cli=MongoClient('mongodb+srv://stenzil:pandey@cluster0-6cu9y.mongodb.net/test?retryWrites=true&w=majority')
+db=cli.get_database('TASK')
+records=db.somedata
+reco=db.org
+col=file.columns
+class post_LAT(BaseModel):
+    key : str
+    place_name : str
+    admin_name1 : str
+    latitude : float
+    longitude: float
+    accuracy : float
+    
+class LAT(BaseModel):
+    longitude: float
+    latitude: float
+
+
+app = FastAPI()
+
+ 
+
+@app.get("/get_location/")
+async def get_location(lat: LAT):
+    record=reco.find_one({"coordinates":[lat.latitude,lat.longitude]})    
+    del record['_id']
+    del record['accuracy']
+    del record['type']
+    del record['coordinates']
+    return record
+    
+@app.post("/post_location/")
+async def post_location(push: post_LAT):
+    a={}
+    record=db.so.find_one({"key":push.key})
+    if record==None: 
+        a[col[0]]=push.key
+        a[col[1]]=push.place_name
+        a[col[2]]=push.admin_name1
+        a["geo"] = {
+               "type" : "Point",
+               "coordinates" : [ 
+                   push.latitude, 
+                   push.longitude
+               ]
+        }
+        a[col[5]]=push.accuracy
+        db.so.insert_one(a)
+        return 'Added'
+    else:
+        return 'Already Exists'
+@app.get("/get_using_postgres/")
+async def get_using_postres(lat: LAT):
+    record=db.so.find({"geo": {
+    "$near": {
+        "$geometry": {
+            "type": "Point" ,
+            "coordinates": [ lat.latitude, lat.longitude ]
+        },
+        "$maxDistance": 5000
+    }
+}})
+    key=[]
+    for i in record:
+        key.append(i['key'])
+    return key
